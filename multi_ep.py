2886fd6ea4f0e6a91b492e7d59c423374b8bffbb
vortacs <jtoscanifield@ucmerced.edu>
2022-02-18 13:56:57 -0800

    changed new alignment building to exclude reference sequence

--- multi_ep.py
+++ multi_ep.py
@@ -45,35 +45,44 @@ def main():
 
         sequence = os.listdir(current_abs_path)[0]
 
-        path_to_align = current_abs_path + "/ep_output/RESULTS/extended.aln"
+        # path_to_align = current_abs_path + "/ep_output/RESULTS/extended.aln"
         path_to_ref_removed_align = current_abs_path + "/" + taxon_dir + "_removed.aln"
 
         # Run EP using each sequence as an input
         subprocess.run([args.ep_path + "/extensiphy.sh", "-a", sequence, "-d", args.reads_dir, "-i", "CLEAN", "-p", str(args.ep_runs) ,"-c", str(args.cores_per_run), "-1", args.tail_1, "-2", args.tail_2, "-o", current_abs_path + "/ep_output"])
 
         # Read alignment and make a copy of the alignment with the reference sequence removed
-        print("Copying the extended EP alignment and removing the reference sequence.")
+        print("Building the extended EP alignment without the reference sequence.")
 
-        align = open(path_to_align, 'r').read()
+        # align = open(path_to_align, 'r').read()
+        #
+        # split_align = align.split(">")
+        #
+        # output = open(path_to_ref_removed_align, 'w')
+        #
+        # for chunk in split_align:
+        #     if len(chunk) > 0:
+        #         if taxon_dir not in chunk:
+        #             output.write(">")
+        #             output.write(chunk)
+        #             output.write("\n")
+        #
+        # output.close()
 
-        split_align = align.split(">")
+        path_to_new_seqs = absolute_output_dir_path + "/" + taxon_dir + "/ep_output/combine_and_infer"
+
+        list_of_new_seqs = os.listdir(path_to_new_seqs)
 
         output = open(path_to_ref_removed_align, 'w')
 
-        for chunk in split_align:
-            if len(chunk) > 0:
-                if taxon_dir not in chunk:
-                    output.write(">")
-                    output.write(chunk)
-                    output.write("\n")
-
-        output.close()
+        for file in list_of_new_seqs:
+            if file.endswith(".fas"):
+                seq = open(path_to_new_seqs + "/" + file, 'r').read()
+                output.write(seq)
+                output.write("\n")
 
 
         os.chdir(absolute_output_dir_path)
-
-
-
 
 
 
